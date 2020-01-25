d03b11986b6d952157b35bb918d24b180f19c90e
Parth Sareen <parth0612@live.com>
2020-01-25 17:19:27 -0500

    Added histogram and curve

---
+++ logic.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import math
+
+def organize_data(data):
+    time = []
+    for line in data:
+        line = line.split()
+        if len(line) == 8:
+            try:
+                time.append(float(line[6].split('time=')[1]))
+            except:
+                print(line[6])
+    return time
+
+def plot_histogram(data_list):
+    x = np.array(data_list)
+    mu = np.mean(x)
+    variance = np.var(x, dtype=np.float64)
+    sigma = math.sqrt(variance)
+
+    fig, ax = plt.subplots()
+    num_bins = 100
+    n, bins, patches = ax.hist(x, num_bins, density=1)
+
+    y = ((1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+    ax
+    ax.plot(bins, y, '--')
+    ax.set_xlabel('Time in ms')
+    ax.set_ylabel('Relative Frequency')
+    ax.set_title(r'Histogram of Pinging Eduroam or smth')
+    fig.tight_layout()
+    plt.show()
+
+def plot_curve(data_list):
+    x = np.array(data_list)
+
+    mean = np.mean(x)
+    variance = np.var(x, dtype=np.float64)
+    sigma = math.sqrt(variance)
+    plt.plot(x, stats.norm.pdf(x, mean, sigma))
+    plt.show()
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from file_funcs import *
 from constants import *
+from logic import *
 
 def main():
 	data = open_file(PATH_TO_DATA)
-	print(data)
+	data_list = organize_data(data)
+	print(data_list)
+	plot_histogram(data_list)
+	#plot_curve(data_list)
+
 
 if __name__ == '__main__':
 	main()
